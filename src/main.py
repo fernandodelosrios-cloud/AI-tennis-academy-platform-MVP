c8842bef030af5bfe8321a31d77fb5fa9887c327
Fernando de los Rios <fernandodlrb@gmail.com>
2026-06-20 23:28:29 +0200

    debug: add global exception handler

--- src/main.py
+++ src/main.py
@@ -29,8 +29,9 @@ from src.connectors.whoop_connector import WhoopConnector
 app = FastAPI(title="TennisIQ MVP API", version="0.2.0")
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True,
                    allow_methods=["*"], allow_headers=["*"])
-import traceback
+
 from fastapi.responses import JSONResponse
+import traceback
 
 @app.exception_handler(Exception)
 async def global_exception_handler(request, exc):
